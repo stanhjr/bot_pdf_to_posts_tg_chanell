385a43507dd2422c93a2505a03e706a56d6ce855
stanhjr <88712960+stanhjr@users.noreply.github.com>
2021-11-03 16:34:03 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,65 @@
+import os
+import time
+import sqlite3
+from aiogram.dispatcher import Dispatcher
+from aiogram.types import InputMediaPhoto
+from aiogram.utils import executor
+from aiogram import Bot, types
+from pdf2image import convert_from_path
+from config import TOKEN
+from config import CHANNEL
+
+connection = sqlite3.connect(r'D:\python\0_bar_to_pdf\bot_examp\test.db')
+cursor = connection.cursor()
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message):
+    fence = '=' * 32
+    cursor.execute("SELECT * FROM pd_test;")
+    one_result = cursor.fetchall()
+    for row in one_result:
+        time.sleep(60)
+        project = f'Номер проекта {row[1]}'
+        order = f'Номер заказа {row[2]}'
+        image_path = row[3]
+        message_pp = f'{fence}\n {project}\n{fence}\n{order}\n{fence}'
+        await bot.send_message(CHANNEL, message_pp)
+
+        pages = convert_from_path(image_path, 150)
+
+        media = []
+
+        for i, page in enumerate(pages):
+            print(len(pages))
+            path_jpg = f'{order}{i}.jpg'
+            page.save(path_jpg, 'JPEG')
+            img = open(path_jpg, 'rb')
+            if len(pages) == 1:
+                await bot.send_photo(CHANNEL, img)
+                break
+            elif i <= 10:
+                print(path_jpg)
+                media.append(InputMediaPhoto(img))
+            else:
+                await bot.send_photo(CHANNEL, img)
+        if len(pages) > 1:
+            await bot.send_media_group(CHANNEL, media=media)
+
+        for i, page in enumerate(pages):
+            path_jpg = f'{order}{i}.jpg'
+            os.remove(path_jpg)
+
+    connection.commit()
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await bot.send_message(message.chat.id, message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
